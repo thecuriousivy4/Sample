3ec0f19bf2ca24745a7caecdab75b9613e3fc877
Ivy <nguyenphanll123@gmail.com>
2024-11-24 18:03:49 +0700

    all commit

---
+++ Car.java
@@ -0,0 +1,9 @@
+public class Car {
+    private int ID;
+    private String brand;
+    private String model;
+    private String color;
+    private int year;
+    private double price;
+    private boolean available;
+}
